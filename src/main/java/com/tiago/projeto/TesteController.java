d3e1e686f0f4dfc1d418e3ffa119172d66221ea4
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2025-03-10 01:57:55 -0300

    feat: Atualizado pastas

--- src/main/java/com/tiago/projeto/TesteController.java
+++ src/main/java/com/tiago/projeto/TesteController.java
@@ -2,13 +2,27 @@ package com.tiago.projeto;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TesteController {
+
     @RequestMapping(path = "/home")
-    public String acao(Model model){
+    public String acao(Model model) {
         model.addAttribute("nome", "Tiago Nascimento Hilario");
         return "index";
+    }
+
+    @GetMapping("/formulario")
+    public String exibiFormulario() {
+        return "form";
+    }
+
+    @PostMapping("/formulario")
+    public String enviaFormulario( Cliente cliente) {
+        System.out.println(cliente);  
+        return "form";
     }
 }
